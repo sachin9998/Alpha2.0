5c80bc4db6e151d8cc46187861dac2e1f55ea507
Sachin Alam <sachinalam4216@gmail.com>
2022-12-10 11:34:37 +0530

    10 Dec Commit

---
+++ Functions/introFunctions.java
@@ -0,0 +1,9 @@
+public class introFunctions {
+
+  public static void main(String[] args) {
+
+    // Intro to functions
+
+  }
+
+}
--- Java Basics/10. Loops/checkPrime.java
+++ Java Basics/10. Loops/checkPrime.java
@@ -5,15 +5,24 @@ public class checkPrime {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-
     int n = scan.nextInt();
 
-    boolean isPrime = false;
+    boolean isPrime = true;
 
     for (int i = 2; i < n - 1; i++) {
 
+      if (n % i == 0) {
+        isPrime = false;
+        break;
+      }
+
+    }
+
+    if (isPrime) {
+      System.out.println("The given number is prime number");
+    } else {
+      System.out.println("Its not a prime number");
     }
 
   }
-
 }
