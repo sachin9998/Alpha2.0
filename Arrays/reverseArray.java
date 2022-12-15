b2703d825f121d167c636b9dd5abe45bd5762bd8
Sachin Alam <sachinalam4216@gmail.com>
2022-12-15 20:02:55 +0530

    15 dec

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -1,6 +1,29 @@
-// public class reverseArray {
-//   public static void main(String[] args) {
+public class reverseArray {
 
-//   }
-// }
+  public static void reverse_array(int arr[]) {
 
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start < end) {
+      int temp = arr[end];
+      arr[end] = arr[start];
+      arr[start] = temp;
+
+      start++;
+      end--;
+    }
+
+  }
+
+  public static void main(String[] args) {
+
+    int arr[] = { 2, 4, 6, 8, 10 };
+    reverse_array(arr);
+
+    for (int i : arr) {
+      System.out.println(i);
+    }
+
+  }
+}
