0246e5b7881352cf1aa325ff613d40cd56fde9c1
Sachin Alam <sachinalam4216@gmail.com>
2022-12-17 09:47:55 +0530

    New 17Dec

---
+++ Arrays/pairs_in_array.java
@@ -0,0 +1,20 @@
+public class pairs_in_array {
+
+  public static void make_Pairs(int arr[]) {
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        System.out.print("(" + arr[i] + ", " + arr[j] + ") ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+
+    // Pairs in Array
+    int myArr[] = { 2, 4, 6, 8, 10 };
+    make_Pairs(myArr);
+
+  }
+}
