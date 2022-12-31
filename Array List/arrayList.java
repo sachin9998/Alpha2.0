366945130cf3aa53db07890b4243b19449a45ada
Sachin Alam <sachinalam4216@gmail.com>
2022-12-31 13:03:06 +0530

    31Dec 2022

---
+++ Array List/arrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class arrayList {
+  public static void main(String[] args) {
+
+    // Declaring ArrayList
+    // String | Boolean | Float
+    ArrayList<Integer> arr = new ArrayList<>();
+
+
+    // Allocating ArrayList
+
+
+
+  }
+
+}
